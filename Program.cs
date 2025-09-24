c3bb0a2f8606a330e6f6621385731d8c20383def
BismaBhatti <bismabhatti512@gmail.com>
2025-09-24 10:01:40 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+public class Countdown
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("Hey Enter your Name: ");
+        string username = Console.ReadLine();
+
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+
+            if (i == 5)
+            {
+                Thread.Sleep(500);
+            }
+
+            else
+            {
+                Thread.Sleep(1000);
+            }
+          
+
+        }
+        Console.WriteLine($"Welcome {username}");
+    }
+
+
+}
